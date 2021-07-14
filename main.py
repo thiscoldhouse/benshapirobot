d5e79dacc27e2553c3d700f4bcb6a4fbe964c282
aleruiz <ales.email@gmail.com>
2021-07-14 12:39:34 -0400

    Some text/formatting updates

--- main.py
+++ main.py
@@ -11,19 +11,17 @@ QUOTES = [
     "Since nobody seems willing to state the obvious due to cultural sensitivity... I’ll say it: rap isn’t music",
     'Rappers have the life expectancies of fruit flies: by the time they’re 40 – if they hit 40 – there’s a good shot they’ll have shot somebody, been shot, been busted for hard core drugs, or acquired an STD',
     ' Palestinian Arabs have demonstrated their preference for suicide bombing over working toilets.',
-    ' The Palestinian people, who support, fund and execute suicide bombings. The Palestinian people, who dress their toddlers in bomb belts and then take family snapshots.',
+    'The Palestinian people, who dress their toddlers in bomb belts and then take family snapshots.',
     'The Palestinian Arab population is rotten to the core.',
     'There is no doubt that law enforcement should be heavily scrutinizing the membership and administration of mosques.',
-    'We can only fight those who share an ideology [Islam] dedicated to our destruction.',
-    ' Palestinian Arabs must be fought on their own terms: as a people dedicated to an evil cause.',
     'If you believe that the Jewish state has a right to exist, then you must allow Israel to transfer the Palestinians and the Israeli-Arabs from Judea, Samaria, Gaza and Israel proper. It’s an ugly solution, but it is the only solution… It’s time to stop being squeamish.'
 ]
 
 
 TEMPLATE = """
-I saw that you mentioned Ben Shapiro. In case you don't know, Ben Shapiro is a grifter and a hack. If you find anything he's said compelling, you should keep in mind he also says things like this:\n\n
+I saw that you mentioned Ben Shapiro. In case you don't know, Ben Shapiro is a grifter and a hack. If you find anything he's said compelling, you should keep in mind he also says things like this:\n\n\n\n
 >{QUOTE}
-\n\n^(I'm BensHapiRobot. My purpose is to contextualize Ben Shapiro to counteract the social media pipeline that sends people his way. I'm part of a project that uses technology to better understand Ben and other right wing grifters. More at /r/AuthoritarianMoment and https://theauthoritarianmoment.com)
+\n\n\n\n^(I'm BensHapiRobot. My purpose is to contextualize Ben Shapiro to counteract the social media pipeline that sends people his way. I'm part of a project that uses technology to better understand Ben and other right wing grifters. More at /r/AuthoritarianMoment and https://theauthoritarianmoment.com)
 """
 
 ALREADY_REPLIED = set()
